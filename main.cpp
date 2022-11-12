1c165902a7305b5afc3a32fcdb398dce34ab2b16
vance <iamvancenet@gmail.com>
2022-11-13 02:09:13 +0330

    Add formula for perimeter and area of square

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 float perimeter ;
 float area ;
-char geometry;
+char geometry[10];
 
 // square
 string n_square = "square";
@@ -15,6 +15,15 @@ float square_sides ;
 cout << "Please enter the type of geometry " << endl ;
 cin >> geometry ;
 cout << endl ; 
+if (geometry == n_square) {
+    cout << "please enter a square sides:" << endl ;
+    cin >> square_sides ;
+    perimeter = square_sides * 4 ;
+    cout << "square perimeter : " << perimeter << endl ;
+    area = square_sides * square_sides ;
+    cout << "square area : " << area << endl ;
+}
+
 return 0 ;
 
 }
